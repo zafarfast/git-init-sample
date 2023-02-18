4cf7e06521efb8180e1f0bb37e0cb9d56f7c6f27
zafarfast <zafar_fast@hotmail.com>
2023-02-18 20:41:25 +1100

    commit

---
+++ lib/db/schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE employee (
+  id INT ,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  role_id INT,
+  manager_id INT
+);
+
+CREATE TABLE role1 (
+    id INT,
+    title VARCHAR(50),
+    salary INT,
+    department_id INT
+    );
+
+CREATE TABLE department (
+  id INT,
+  dept_name VARCHAR(50)
+);
