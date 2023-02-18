3401ba8b34ea03b88115f30f3a37943fb6179877
zafarfast <zafar_fast@hotmail.com>
2023-02-18 22:20:00 +1100

    Added primary and secondary keys

--- lib/db/schema.sql
+++ lib/db/schema.sql
@@ -3,22 +3,33 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE employee (
-  id INT ,
-  first_name VARCHAR(50),
-  last_name VARCHAR(50),
-  role_id INT,
-  manager_id INT
-);
+CREATE TABLE department (
+  id INT PRIMARY KEY,
+  dept_name VARCHAR(50));
 
 CREATE TABLE role1 (
     id INT,
     title VARCHAR(50),
     salary INT,
-    department_id INT
+    department_id INT ,
+    PRIMARY KEY(id),
+    FOREIGN KEY(department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
     );
 
-CREATE TABLE department (
-  id INT,
-  dept_name VARCHAR(50)
+  
+CREATE TABLE employee (
+  id INT ,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  role_id INT,
+  manager_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY(role_id)
+  REFERENCES role1(id)
+  ON DELETE SET NULL
 );
+
+
+
