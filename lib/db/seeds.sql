4df35113bb61de0fe17666f1a271ff9cc56cff8c
zafarfast <zafar_fast@hotmail.com>
2023-02-18 22:39:23 +1100

    fixed empty database issue

--- lib/db/seeds.sql
+++ lib/db/seeds.sql
@@ -1,17 +1,17 @@
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1, "Alice", "Parket",1,4),
-       (2, "Bob", "Dylon",4,3),
-       (3, "Garry", "Kumar",2,4),
-       (4, "Charlie", "Ryan",3, null);
+VALUES (1, "Alice", "Parket",111,4),
+       (2, "Bob", "Dylon",444,3),
+       (3, "Garry", "Kumar",222,4),
+       (4, "Charlie", "Ryan",333, null);
 
 INSERT INTO role1 (id, title, salary, department_id)
-VALUES (1, "Manager", 100000,1),
-       (2, "Assistant", 60000,3),
-       (3, "CEO",250000,2),
-       (4, "CFO",20000,4);
+VALUES (111, "Manager", 100000,11),
+       (222, "Assistant", 60000,33),
+       (333, "CEO",250000,22),
+       (444, "CFO",20000,44);
 
 INSERT INTO department (id, dept_name)
-VALUES (1, "Finance"),
-       (2, "Operations"),
-       (3, "IT"),
-       (4, "Marketing");
+VALUES (11, "Finance"),
+       (22, "Operations"),
+       (33, "IT"),
+       (44, "Marketing");
